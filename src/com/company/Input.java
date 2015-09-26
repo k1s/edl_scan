2f1fca7e544181f8656f5f98c853356e00ca6024
Alex <hattkis@gmail.com>
2015-09-26 20:31:05 +0300

    Input updated

--- src/com/company/Input.java
+++ src/com/company/Input.java
@@ -5,19 +5,7 @@ import java.util.ArrayList;
 
 public class Input {
 
-    public static void main(String[] args) {
-
-        if (args.length != 2) {
-            System.out.println("Enter path to scan and path to EDL. ltoumom!");
-            System.exit(0);
-        }
-
-        String PATH = args[0];
-
-        String EDL = args[1];
-
-        System.out.println("Starting with " + EDL);
-        System.out.println();
+    public ArrayList<String> getInput(final String EDL) {
 
         FileReader input = null;
         try {
@@ -33,32 +21,21 @@ public class Input {
 
         try {
             while ((strIn = bufRead.readLine()) != null)   {
-//                if (strIn != null || strIn.equals("") || strIn.length() > 4) {
-//                    strIn = strIn.substring(5, 16);
                 if (strIn.length() > 1 && strIn.matches("[0-9].*")) {
-                    strDev = strIn.split("\\s");//.substring(sp, strIn.length() - 1).split(" ")[0];
+                    strDev = strIn.split("\\s");
                     strOut.add(strDev[2]);
                 }
 
                 if (strIn.length() > 1 && strIn.matches(".*CLIP NAME.*")) {
-                    strDev = strIn.split("\\s");//.substring(sp, strIn.length() - 1).split(" ")[0];
-                    strOut.add(strDev[2]);
+                    strDev = strIn.split("\\s");
+                    strOut.add(strDev[5]);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(strOut);
-
-//        //String command = "touch " + PATH + "newfile.test";
-//
-//        try {
-//            Runtime.getRuntime().exec(command);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
+        return strOut;
 
     }
 
