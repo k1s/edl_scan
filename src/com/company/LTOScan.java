a693c65fbb64d043d4fd0e24f93f0f6519cf524e
Alex <hattkis@gmail.com>
2015-09-26 21:18:04 +0300

    LTOScan update

--- src/com/company/LTOScan.java
+++ src/com/company/LTOScan.java
@@ -11,14 +11,35 @@ public class LTOScan {
 
     ArrayList<String> filesWalk = new ArrayList<String>();
 
-    public ArrayList<String> getFromLTO(final String PATH) throws LTONotMountedException {
+    public ArrayList<String> getFromLTO() throws LTONotMountedException {
 
-        File root = new File(PATH);
+        File root = new File(LTO);
         File[] list = root.listFiles();
         if (list.length == 0)
             throw new LTONotMountedException();
 
         return walk(LTO);
+
+    }
+
+    public ArrayList<String> customWalk(final String PATH, final ArrayList<String> fromEDL) {
+
+        File root = new File(PATH);
+        File[] list = root.listFiles();
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                walk( f.getAbsolutePath() );
+                if (fromEDL.contains(String.valueOf(f.getAbsoluteFile())))
+                    filesWalk.add(String.valueOf(f.getAbsoluteFile()));
+            }
+            if (f.isFile()) {
+                if (String.valueOf(f.getAbsoluteFile()).matches(".*cine"))
+                    filesWalk.add(String.valueOf(f.getAbsoluteFile()));
+            }
+        }
+
+        return filesWalk;
 
     }
 
