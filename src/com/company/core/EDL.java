6bda5d187efb1fcbd4572bea892f81409015acef
Alex D <hattkis@gmail.com>
2016-01-04 23:58:37 +0300

    EDL update with Java 8

--- src/com/company/core/EDL.java
+++ src/com/company/core/EDL.java
@@ -1,50 +1,51 @@
 package com.company.core;
 
 import java.io.*;
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EDL {
 
-    private static final String SHORT_REEL_NAME = "[0-9].*";
-    private static final String REEL_NAME_FROM_SOURCE_FILE = ".*SOURCE FILE.*";
-    private static final String REEL_NAME_FROM_CLIP_NAME = ".*CLIP NAME.*";
+    private final String SHORT_REEL_NAME = "(?<=\\d\\d\\d)(.*)(?=\\sV)";
+    private final String REEL_NAME_FROM_SOURCE_FILE = "(?<=SOURCE FILE:)(.*)";
+    private final String REEL_NAME_FROM_CLIP_NAME = "(?<=CLIP NAME:)(.*)";
 
-    private static final int SHORT_REEL_NAME_POSITION = 2;
-    private static final int REEL_NAME_FROM_SOURCE_FILE_POSITION = 3;
+    private final Set<String> strOut = new HashSet<>();
+    private List<String> lines;
 
-    public ArrayList<String> getInput(final String EDL, final boolean shortReelNames) {
-
-        Set<String> strOut = new HashSet<>();
-        try (BufferedReader bufRead = new BufferedReader(new FileReader(EDL))) {
-            String strIn;
-            while ((strIn = bufRead.readLine()) != null)   {
-                checkStrings(strIn, strOut, shortReelNames);
-            }
+    public EDL(final Path EDL) {
+        try {
+            this.lines = Files.readAllLines(EDL);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>(strOut);
     }
 
-    private void checkStrings(final String strIn, final Set<String> strOut, final boolean shortReelNames) {
-        if (shortReelNames) {
-            checkString(strIn, SHORT_REEL_NAME, SHORT_REEL_NAME_POSITION, strOut);
-        }
-        checkString(strIn, REEL_NAME_FROM_SOURCE_FILE, REEL_NAME_FROM_SOURCE_FILE_POSITION, strOut);
-        checkString(strIn, REEL_NAME_FROM_CLIP_NAME, REEL_NAME_FROM_CLIP_NAME_POSITION(strIn), strOut);
+    public List<String> getInput(final boolean shortReelNames) {
+        Pattern reelNames = Pattern.compile(SHORT_REEL_NAME);
+        Pattern sourceFiles = Pattern.compile(REEL_NAME_FROM_SOURCE_FILE);
+        Pattern clipNames = Pattern.compile(REEL_NAME_FROM_CLIP_NAME);
+        if (shortReelNames)
+            extractFromLines(reelNames);
+        extractFromLines(sourceFiles);
+        extractFromLines(clipNames);
+
+        return outStrings();
     }
 
-    private int REEL_NAME_FROM_CLIP_NAME_POSITION(final String string) {
-        return string.split("\\s").length-1;
+    private void extractFromLines(Pattern pattern) {
+        this.lines.forEach(line -> {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                this.strOut.add(matcher.group().trim());
+            }
+        });
     }
 
-    private void checkString(final String forCheck, final String matcher, final int position, final Set<String> set) {
-        if (forCheck.length() > 1 && forCheck.matches(matcher)) {
-            String[] strDev = forCheck.split("\\s");
-            int validPositionFromLine = strDev.length-1 < position ? strDev.length-1 : position;
-            if (strDev[validPositionFromLine] != null)
-                set.add(strDev[validPositionFromLine]);
-        }
+    private List<String> outStrings() {
+        return new ArrayList<>(this.strOut);
     }
 }
