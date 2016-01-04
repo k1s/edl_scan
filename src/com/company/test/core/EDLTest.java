37c1f156a3faee1228160f2e9f9be8464305b544
Alex D <hattkis@gmail.com>
2016-01-05 00:18:10 +0300

    EDLTest update

--- src/com/company/test/core/EDLTest.java
+++ src/com/company/test/core/EDLTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -35,15 +36,7 @@ public class EDLTest {
     }
 
     @Test
-    public void out() {
-        URL url = this.getClass().getResource("test.edl");
-        System.out.println(url);
-        assertEquals(url.toString(), outContent.toString());
-    }
-
-    @Test
-    public void testGetInput() throws Exception {
-        EDL testEDL = new EDL();
+    public void testGetInputWithShortReels() throws Exception {
         URL url = this.getClass().getResource("test.edl");
         String correctFilePath = url.getFile().replace("%20", " ");
         List<String> expectedStrings = new ArrayList<>();
@@ -53,7 +46,27 @@ public class EDLTest {
         expectedStrings.addAll(Arrays.asList(expected));
         Collections.sort(expectedStrings);
         boolean useShortReelNames = true;
-        List<String> stringsFromEDL = testEDL.getInput(correctFilePath, useShortReelNames);
+        EDL testEDL = new EDL(Paths.get(correctFilePath));
+        List<String> stringsFromEDL = testEDL.getInput(useShortReelNames);
+        Collections.sort(stringsFromEDL);
+        assertEquals(expectedStrings, stringsFromEDL);
+
+
+    }
+
+    @Test
+    public void testGetInputWithoutShortReels() throws Exception {
+        URL url = this.getClass().getResource("test.edl");
+        String correctFilePath = url.getFile().replace("%20", " ");
+        List<String> expectedStrings = new ArrayList<>();
+        String[] expected = new String[]{"A066C004_150602_R1KA", "MVI_4321",
+                "C070_C001_0715GR", "A051C015_141126_R56W",
+                "A050C003_141126_R56W", "ST5A3328.MOV"};
+        expectedStrings.addAll(Arrays.asList(expected));
+        Collections.sort(expectedStrings);
+        boolean useShortReelNames = false;
+        EDL testEDL = new EDL(Paths.get(correctFilePath));
+        List<String> stringsFromEDL = testEDL.getInput(useShortReelNames);
         Collections.sort(stringsFromEDL);
         assertEquals(expectedStrings, stringsFromEDL);
     }
