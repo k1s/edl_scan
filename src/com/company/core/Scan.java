24b2ef0bba5079264002ac8f87b9030138a4950c
Alex D <hattkis@gmail.com>
2016-01-04 23:59:27 +0300

    Scan update with J8

--- src/com/company/core/Scan.java
+++ src/com/company/core/Scan.java
@@ -3,105 +3,67 @@ package com.company.core;
 import com.company.exceptions.NotMountedException;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class Scan {
 
-    public ArrayList<String> getFromSource(final ArrayList<String> fromEDL, final String source) throws NotMountedException {
-        File root = new File(source);
+    private final List<String> fromEDL;
+    private final Path source;
+
+    public Scan(List<String> fromEDL, Path source) {
+        this.fromEDL = fromEDL.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+        this.source = source;
+    }
+
+    public List<Path> getFromSource(final boolean checkFiles) throws NotMountedException {
+        File root = new File(String.valueOf(source));
         if (root.list().length == 0)
             throw new NotMountedException();
-        ArrayList<String> filesWalk = new ArrayList<String>();
-        return customWalk(fromEDL, source, filesWalk);
+        Set<Path> filesWalk = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        return customWalk(source, filesWalk, checkFiles);
     }
 
-    public ArrayList<String> customWalk(final ArrayList<String> fromEDL, final String source, final ArrayList<String> filesWalk) {
+    private List<Path> customWalk(final Path source, final Set<Path> filesWalk, final boolean checkFiles) {
 
-        File root = new File(source);
-        File[] list = root.listFiles();
-        List<File> files = new ArrayList<>();
-        if (list != null) {
-            files = Arrays.asList(list);
-        }
-        files.parallelStream().filter(f -> !f.getAbsolutePath().contains(".DS_Store")).forEach(f -> {
-            String fileName = String.valueOf(f.getAbsoluteFile());
-            if ( f.isDirectory() ) {
-                checkDirName(fileName, filesWalk, fromEDL);
-                customWalk(fromEDL, f.getAbsolutePath(), filesWalk);
-            }
-            if (f.isFile()) {
-                checkFileName(fileName, filesWalk, fromEDL);
-            }
-        });
-
-        return filesWalk;
-
-    }
-
-    public ArrayList<String> checkScan(final ArrayList<String> fromEDL, final String source) throws NotMountedException {
-
-        File root = new File(source);
-        File[] list = root.listFiles();
-        if (list != null)
-            if (list.length == 0)
-                throw new NotMountedException();
-        ArrayList<String> strings = new ArrayList<String>();
-
-        return checkScanWalk(fromEDL, source, strings);
-
-    }
-
-    public ArrayList<String> checkScanWalk(final ArrayList<String> fromEDL, final String source, final ArrayList<String> strings) {
-
-
-        File root = new File(source);
-        File[] list = root.listFiles();
-        List<File> files = new ArrayList<>();
-        if (list != null) {
-            files = Arrays.asList(list);
+        try {
+            Files.list(source)
+                    .parallel()
+                    .filter(path -> !path.toString().contains(".DS_Store"))
+                    .forEach(path -> {
+                        if (Files.isDirectory(path)) {
+                            checkDirName(path, filesWalk);
+                            customWalk(path, filesWalk, checkFiles);
+                        }
+                        if (Files.isRegularFile(path)) {
+                            checkFileName(path, filesWalk, checkFiles);
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        files.parallelStream().filter(f -> !f.getAbsolutePath().contains(".DS_Store")).forEach(f -> {
-            //TODO one function for all of them
-            String fileName = String.valueOf(f.getAbsoluteFile());
-            if ( f.isDirectory() ) {
-                checkName(fileName, strings, fromEDL);
-                checkScanWalk(fromEDL, f.getAbsolutePath(), strings);
-            }
-            if (f.isFile()) {
-                //TODO avoid check arri
-//                checkName(fileName, strings, fromEDL);
-            }
-        });
+        return new ArrayList<>(filesWalk);
 
-        return strings;
     }
 
-    private void checkName(final String fileName, final ArrayList<String> strings, final ArrayList<String> fromEDL) {
-        fromEDL.parallelStream().forEach(s -> {
-            if ((fileName.toLowerCase().contains(s.toLowerCase()) || fileName.contains(s)) && !strings.contains(s))
-                strings.add(s);
-        });
-    }
-
-    private void checkDirName(final String fileName, final ArrayList<String> filesWalk, final ArrayList<String> fromEDL) {
-        String toCheck = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
-        fromEDL.parallelStream().forEach(s -> {
-            if (toCheck.contains(s) || toCheck.toLowerCase().contains(s.toLowerCase()))
+    private void checkDirName(final Path fileName, final Set<Path> filesWalk) {
+        fromEDL.forEach(str -> {
+            if (fileName.getFileName().toString().toLowerCase().contains(str))
                 filesWalk.add(fileName);
         });
     }
 
-    private void checkFileName(final String fileName, final ArrayList<String> filesWalk, final ArrayList<String> fromEDL) {
-        String toCheck = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
-        //TODO ADD OPTION TO CHECK FILES LIKE .MOV
-        List<String> fromEDLLow = fromEDL.parallelStream().map(String::toLowerCase).collect(Collectors.toList());
-        if (fromEDL.contains(toCheck) || fromEDLLow.contains(toCheck.toLowerCase()))
+    private void checkFileName(final Path fileName, final Set<Path> filesWalk, final boolean checkFiles) {
+        if (fromEDL.contains(fileName.getFileName().toString().toLowerCase()))
             filesWalk.add(fileName);
-
+        if (checkFiles)
+            checkDirName(fileName, filesWalk);
     }
 
 }
