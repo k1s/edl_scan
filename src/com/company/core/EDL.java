3298c03216558cf0b98205273d6085fb170c3e97
Alex D <hattkis@gmail.com>
2016-01-07 02:35:00 +0300

    EDL update

--- src/com/company/core/EDL.java
+++ src/com/company/core/EDL.java
@@ -1,9 +1,12 @@
 package com.company.core;
 
+import com.company.DRY.Assert;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +16,11 @@ public class EDL {
     private final String REEL_NAME_FROM_SOURCE_FILE = "(?<=SOURCE FILE:)(.*)";
     private final String REEL_NAME_FROM_CLIP_NAME = "(?<=CLIP NAME:)(.*)";
 
-    private final Set<String> strOut = new HashSet<>();
+    private volatile Set<String> strOut = Collections.newSetFromMap(new ConcurrentHashMap<>());
     private List<String> lines;
 
     public EDL(final Path EDL) {
+        Assert.require(path -> Files.exists(path), EDL, "Path to EDL does not exist");
         try {
             this.lines = Files.readAllLines(EDL);
         } catch (IOException e) {
@@ -28,22 +32,27 @@ public class EDL {
         Pattern reelNames = Pattern.compile(SHORT_REEL_NAME);
         Pattern sourceFiles = Pattern.compile(REEL_NAME_FROM_SOURCE_FILE);
         Pattern clipNames = Pattern.compile(REEL_NAME_FROM_CLIP_NAME);
+        ExecutorService executorService = Executors.newCachedThreadPool();
         if (shortReelNames)
-            extractFromLines(reelNames);
-        extractFromLines(sourceFiles);
-        extractFromLines(clipNames);
-
+            executorService.execute(new Thread(() -> extractFromLines(reelNames)));
+        executorService.execute(new Thread(() -> extractFromLines(clipNames)));
+        executorService.execute(new Thread(() -> extractFromLines(sourceFiles)));
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return outStrings();
     }
 
     private void extractFromLines(Pattern pattern) {
-        System.out.println(pattern.toString());
-        this.lines.forEach(line -> {
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.find()) {
-                this.strOut.add(matcher.group().trim());
-            }
-        });
+                this.lines.forEach(line -> {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        this.strOut.add(matcher.group().trim());
+                    }
+                });
     }
 
     private List<String> outStrings() {
