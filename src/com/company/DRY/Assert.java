7e119f8c1c01485a3828df294d0c16c536a31090
Alex D <hattkis@gmail.com>
2016-01-11 18:46:16 +0300

    Assert update

--- src/com/company/DRY/Assert.java
+++ src/com/company/DRY/Assert.java
@@ -1,5 +1,7 @@
 package com.company.DRY;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Predicate;
 
 public final class Assert {
@@ -21,5 +23,10 @@ public final class Assert {
     public static <T> void require(Predicate<T> predicate, T testVal, String message) {
         if (!predicate.test(testVal))
             throw new IllegalArgumentException(message);
+    }
+
+    public static void requirePath(Path path) {
+        if (!Files.exists(path))
+            throw new IllegalArgumentException("Path does not exist");
     }
 }
